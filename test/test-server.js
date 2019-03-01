4cde2dbdb79f3ac7e8b75162b1db9e46d4049b25
cymruhangu <pragquix@gmail.com>
2019-02-28 21:12:22 -0500

    added before after hooks to start and stop server

--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,19 @@
 const chai = require('chai');
- const chaiHttp = require('chai-http');
+const chaiHttp = require('chai-http');
 
- const {app} = require('../server');
-
+ const { app, runServer, closeServer } = require('../server');
+ const {TEST_DATABASE_URL} = require('../config');
  const should = chai.should();
  chai.use(chaiHttp);
 
  describe('API', function() {
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+  after(function(){
+    return closeServer()
+  });
 
    it('should 200 on GET requests', function() {
      return chai.request(app)
