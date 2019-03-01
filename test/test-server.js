34b2133a118e8cfb9907e2bf113591f0b05a2368
cymruhangu <pragquix@gmail.com>
2019-02-28 20:30:24 -0500

    changed endpoint on test-server.js

--- test/test-server.js
+++ test/test-server.js
@@ -10,7 +10,7 @@ const chai = require('chai');
 
    it('should 200 on GET requests', function() {
      return chai.request(app)
-       .get('/api/projects')
+       .get('/projects')
        .then(function(res) {
          res.should.have.status(200);
          res.should.be.json;
