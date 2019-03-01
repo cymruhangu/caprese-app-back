4232f263ef519b3c4beed02f277b6932374a701e
cymruhangu <pragquix@gmail.com>
2019-02-28 19:20:35 -0500

    modified .travis.yml

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/caprese-app2';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://mkim:passw0rd!@ds139951.mlab.com:39951/caprese-app';
 exports.TEST_DATABASE_URL = process.env.DATABASE_URL || 'mongodb://mkim:passw0rd!@ds139951.mlab.com:39951/caprese-app-test';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 3080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
